9fd036de3510d97a62576260170eef56eacbd9f2
daichi-appy <daichi.es47@gmail.com>
2025-04-28 17:25:06 +0900

    動きと判定追加

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let squares = Array.from(document.querySelectorAll(".grid div"));
   const scoreDisplay = document.querySelector("#score");
   const startBtn = document.querySelector('#start-button')
+  const restartButton = document.getElementById('restart-button')
 
   const width = 10
   let nextRandom = 0
@@ -31,7 +32,49 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   }
 
-  startBtn.addEventListener('click',  () => {
+  function moveDown() {
+    undraw()
+    currentPosition += width
     draw()
+    freeze()
+  }
+
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+      squares[currentPosition + index].style.backgroundColor = ''
+    })
+  }
+
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+      // displayShape()
+      gameOver()
+    }
+  }
+
+  function gameOver() {
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      scoreDisplay.innerHTML = 'end'
+      clearInterval(timerId)
+      restartButton.addEventListener('click', function() { location.reload() })
+    }
+  }
+
+  startBtn.addEventListener('click',  () => {
+    if (timerId) {
+      clearInterval(timerId)
+      timerId = null
+    } else {
+      draw()
+      timerId = setInterval(moveDown, 100)
+    }
   })
 })
