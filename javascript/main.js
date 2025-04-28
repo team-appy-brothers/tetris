6e338ca8342948d0d0b59033c3ea1d0425e4ba83
KuriharaDesu <kurihara_takashi@andfactory.co.jp>
2025-04-28 18:58:51 +0900

    Revert "埋まった行の削除＆スコア計算"
    This reverts commit c18045c2bff4f4b1e14eb51780cc44245e21ead8.

--- javascript/main.js
+++ javascript/main.js
@@ -56,14 +56,13 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4
       draw()
       // displayShape()
-      addScore()
       gameOver()
     }
   }
 
   function gameOver() {
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      scoreDisplay.innerHTML = score
+      scoreDisplay.innerHTML = 'end'
       clearInterval(timerId)
       restartButton.addEventListener('click', function() { location.reload() })
     }
@@ -148,23 +147,4 @@ document.addEventListener("DOMContentLoaded", () => {
       timerId = setInterval(moveDown, 100)
     }
   })
-
-  function addScore() {
-    for (let i = 0; i < 199; i += width) {
-      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
-
-      if (row.every(index => squares[index].classList.contains('taken'))) {
-        score += 10
-        scoreDisplay.innerHTML = score
-        row.forEach(index => {
-          squares[index].classList.remove('taken')
-          squares[index].classList.remove('tetromino')
-          squares[index].style.backgroundColor = ''
-        })
-        const squaresRemoved = squares.splice(i, width)
-        squares = squaresRemoved.concat(squares)
-        squares.forEach(cell => grid.appendChild(cell))
-      }
-    }
-  }
 })
